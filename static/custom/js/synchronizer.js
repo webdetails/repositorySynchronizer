9ad0d7bc31b1502b353f978f092bdb99150a116f
Bruno Silva <bruno.silva@webdetails.com>
2013-11-11 20:13:12 +0000

    dialogs (CSS + tables) other functionalities

--- static/custom/js/synchronizer.js
+++ static/custom/js/synchronizer.js
@@ -36,6 +36,7 @@ var synchronizer = {};
 	    $("#dataCell2").toggleClass("collapsed");
 	}
 */
+
   	myself.createTableEmptyRawData = function() {
     	var emptyData = {
       		metadata:[
@@ -130,7 +131,46 @@ var synchronizer = {};
 		return (status == "N" ? "Y" : "N");
 	}
 
-	myself.getEndpointCaller = function( pluginId, endpoint, opts ){
+	myself.getColIndexFromColName = function( metadata, name ) {
+		var colIndex;
+		$.each(metadata, function(idx,ele){
+			if(ele.colName === name){
+				colIndex = ele.colIndex;
+			}
+		});
+		return colIndex;
+	}
+	myself.tablePostChangeProcedure = function( data, fileCounterParam , files2deleteParam , destinationRepoLocation) {
+
+		/* actionName should be one of the following: jcr2fs , synch , fs2jcr */
+
+	    if(_.isEmpty(data)){
+	        data = synchronizer.createTableEmptyRawData();
+	    } else {
+		    var files2deleteList = [],
+		        fileCounter = 0,
+		        statusIdx = synchronizer.getColIndexFromColName(data.metadata,"modification_status"),
+		        fileIdx = synchronizer.getColIndexFromColName(data.metadata,"file"),
+		        typeIdx = synchronizer.getColIndexFromColName(data.metadata,"type");
+		    /* Update respective ToBeDeletedList and FileCounterParam */
+		    $.each(data.resultset,function(idx,ele){
+		        if(ele[typeIdx] === "file"){
+		            fileCounter += 1;
+		            if(ele[statusIdx] === "to_be_deleted"){
+		                files2deleteList.push(ele[fileIdx]);
+		            }
+		        }
+		    });
+		    Dashboards.setParameter(files2deleteParam,files2deleteList);
+		    Dashboards.fireChange(fileCounterParam,fileCounter);
+
+		    /* Update file and modification_status headers*/
+		    data.metadata[fileIdx].colName = Dashboards.getParameterValue(destinationRepoLocation);
+	    	data.metadata[statusIdx].colName = "Modification status";
+	    }
+	    return data;
+	};
+/*	myself.getEndpointCaller = function( pluginId, endpoint, opts ){
 	    var myself = this;
 	    
 	    return function (callback, errorCallback, params){
@@ -141,5 +181,5 @@ var synchronizer = {};
 	      	myself.runEndpoint(pluginId, endpoint, _opts);
 	    }
 	};
-
+*/
 })(synchronizer);
